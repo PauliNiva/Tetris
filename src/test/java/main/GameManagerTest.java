5bdd8ffa2ab6826aeed1c50ede277a7ce4bc0766
Pauli Niva <pauli.niva@helsinki.fi>
2015-05-27 15:50:50 +0300

    GUITesting and travis with xvfb attempt

---
+++ src/test/java/main/GameManagerTest.java
@@ -0,0 +1,21 @@
+package main;
+
+
+import applogic.PlayingField;
+import gui.PlayingFieldGUI;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.swing.*;
+
+public class GameManagerTest {
+
+    GameManager gameManager;
+
+    @Test
+    public void GameManagerInitializesPlayingFieldCorrectly() throws InterruptedException {
+        gameManager = new GameManager();
+        PlayingFieldGUI testField = gameManager.getPlayingField();
+        Assert.assertEquals(200, testField.getPlayingField().getRows() * testField.getPlayingField().getColumns());
+    }
+}
